613e3ce6b6dd5014a4df033f01ac7a67c9371613
Joshua <joshua.bogart.ndt@gmail.com>
2020-07-30 18:55:28 -0500

    nukeing our repository and starting from project 2 set up :)

--- passportdemo.sql
+++
@@ -1,4 +0,0 @@
-DROP DATABASE IF EXISTS passport_demo_db;
-CREATE database passport_demo_db;
-
-USE passport_demo_db;
--- server.js
+++ server.js
@@ -1,30 +1,23 @@
-// Requiring necessary npm packages
-var express = require("express");
-var session = require("express-session");
-// Requiring passport as we've configured it
-var passport = require("./config/passport");
+const express = require('express');
+const routes = require('./controllers');
+const app = express();
+const PORT = process.env.PORT || 3001;
+var db = require('./models');
 
-// Setting up port and requiring models for syncing
-var PORT = process.env.PORT || 8080;
-var db = require("./models");
-
-// Creating express app and configuring middleware needed for authentication
-var app = express();
+// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
-// We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+// Add routes, both API and view
+app.use(routes);
 
-// Requiring our routes
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
-
-// Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+// Start the API server
+// ADD SEQUELIZE HERE TO CONNECT TO YOUR DB
+db.sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
   });
 });
