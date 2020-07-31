1be739f76bb768fb8670dd45499869ab04594e10
Joshua <joshua.bogart.ndt@gmail.com>
2020-07-30 20:10:51 -0500

    added local db, updated dev config, and updated .json file

---
+++ scripts/seedDB.sql
@@ -0,0 +1,29 @@
+/*HERE IS WHERE YOUR SQL SCRIPTS WILL BE PLACED*/
+DROP DATABASE IF EXISTS triggeredDB;
+CREATE database triggeredDB;
+
+USE triggeredDB;
+
+CREATE TABLE users (
+  position INT NOT NULL,
+  username VARCHAR(10) NULL,
+  user_image VARCHAR(200) NULL,
+  email VARCHAR(100) NULL,
+  password_KEY VARCHAR(10) NULL,
+  votes INT NULL,
+  user_comments INT NULL,
+  PRIMARY KEY (position)
+);
+
+CREATE TABLE debates (
+  position INT NOT NULL,
+  incumbent_username VARCHAR(10) NOT NULL,
+  challenger_username VARCHAR(10) NULL,
+  topic VARCHAR(100) NOT NULL,
+  incumbent_body VARCHAR (280) NOT NULL,
+  chalenger_body VARCHAR (280) NULL,
+  incumbent_votes INT NULL,
+  challenger_votes INT NULL,
+  comments VARCHAR (280) NULL,
+  PRIMARY KEY (position)
+);
