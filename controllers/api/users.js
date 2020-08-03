42c7d938ab4dd81f8fea4a5b960154e70c971e8a
Joshua <joshua.bogart.ndt@gmail.com>
2020-08-03 16:18:26 -0500

    updeted get login

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,89 +1,25 @@
 const router = require("express").Router();
 var db = require("../../models");
-// get route -> index
-router.get("/", function(req, res) {
-  //do things here for other routes
-});
 
-// // get route, for log in
-// router.get("/user", function(req, res) {
-//   // replace old function with sequelize function
-//   db.User.findAll({
-//     include: [db.User],
-//     // Here we specify we want to return our burgers in ordered by ascending burger_name
-//     order: [["username", "ASC"]],
-//   })
-
-//     // use promise method to pass the burgers...
-//     .then(function(dbUser) {
-//       // into the main index, updating the page
-//       var hbsObject = {
-//         user: dbUser,
-//       };
-//       return res.render("index", hbsObject);
-//     });
-// });
-
-// post route to creat user
-router.post("/user", function(req, res) {
-  // edited burger create to add in a burger_name
-  db.User.create({
-    username: req.body.username,
-  }),
-  db.User.create({
+// post a user , route => ('api/user')
+router.post("/", function (req, res) {
+  db.Users.create({
+    username: req.body.usernmae,
     email: req.body.email,
-  }),
-  db.User.create({
-    password_KEY: req.body.password_KEY,
-  })
-    // pass the result of our call
-    .then(function(User) {
-      // log the result to our terminal/bash window
-      console.log(User);
-      // redirect
-      res.redirect("/");
-    });
+    password: req.body.password,
+  }).then(function (dbCreateUser) {
+    res.json(dbCreateUser);
+  });
 });
 
-// // put route to devour a burger
-// router.put("/burgers/update", function(req, res) {
-//   // If we are given a customer, create the customer and give them this devoured burger
-//   if (req.body.customer) {
-//     db.Customer.create({
-//       customer: req.body.customer,
-//       BurgerId: req.body.burger_id,
-//     })
-//       .then(function(dbCustomer) {
-//         return db.Burger.update(
-//           {
-//             devoured: true,
-//           },
-//           {
-//             where: {
-//               id: req.body.burger_id,
-//             },
-//           },
-//         );
-//       })
-//       .then(function(dbBurger) {
-//         res.json("/");
-//       });
-//   }
-//   // If we aren"t given a customer, just update the burger to be devoured
-//   else {
-//     db.Burger.update(
-//       {
-//         devoured: true,
-//       },
-//       {
-//         where: {
-//           id: req.body.burger_id,
-//         },
-//       },
-//     ).then(function(dbBurger) {
-//       res.json("/");
-//     });
-//   }
-// });
+// get a a user route => ('api/user')
+router.get ("/", function (req, res) {
+  db.Users.get({
+    username: req.body.usernmae,
+    password: req.body.password
+  }).then(function (dbLoginUser) {
+    res.json(dbLoginUser);
+  });
+});
 
 module.exports = router;
