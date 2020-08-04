651ae9582c94873c09f6b8b7fe9456a996de32da
Joshua <joshua.bogart.ndt@gmail.com>
2020-08-03 20:04:51 -0500

    funally got our login and user rout connected, will build the actual data handling next

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import './App.css';
-import Login from './pages/Login/Login';
-import Account from './pages/Account/Account';
-import Debate from './pages/Debate/Debate';
+import React from "react";
+import "./App.css";
+import Login from "./pages/Login/Login";
+import Account from "./pages/Account/Account";
+import Debate from "./pages/Debate/Debate";
 // import NewDebates from './pages/NewDebates/NewDebates';
 // import Trending from './pages/Trending/Trending';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
+//import API from "./utils/api"
 
 function App() {
+  // const [user, setUser] = useState({})
+  // const handleLogin = ()=>{
+  //   API.logIn().then((res)=>{
+  //     //call set
+  //     //update state or hook with user info
+  //   })
+  // }
+
   return (
     <Router>
       <div>
