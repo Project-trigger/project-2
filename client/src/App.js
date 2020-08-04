6786561fdfc0d7424d4a8ef1bf6fd9c46eee7785
Michael Srithapin <michael.srithapin@gmail.com>
2020-08-03 20:56:28 -0500

    merging branches

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,62 @@
-<<<<<<< HEAD
 import React from 'react';
 import './App.css';
 import Login from './pages/Login/Login';
 import Account from './pages/Account/Account';
 import Debate from './pages/Debate/Debate';
-import NewDebates from './pages/NewDebates/NewDebates';
-=======
-import React from "react";
-import "./App.css";
-import Login from "./pages/Login/Login";
-import Account from "./pages/Account/Account";
-import Debate from "./pages/Debate/Debate";
 // import NewDebates from './pages/NewDebates/NewDebates';
 // import Trending from './pages/Trending/Trending';
->>>>>>> a9a75f6d85c82362e158a701377128b9dc0a0d19
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
-//import API from "./utils/api"
+import { Container } from "react-bootstrap";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Footer, Loading } from "./components";
+import { Profile, ExternalApi } from "./pages";
+import "./app.css";
 
-function App() {
-  // const [user, setUser] = useState({})
-  // const handleLogin = ()=>{
-  //   API.logIn().then((res)=>{
-  //     //call set
-  //     //update state or hook with user info
-  //   })
-  // }
+const App = () => {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <main>
+    <div id="app" className="d-flex flex-column h-100">
+      <Navbar />
+      <Container className="flex-grow-1 mt-5">
+        <Switch>
+          {/* <Route path="/" exact component={Home} /> */}
+          <Route path="/profile" component={Profile} />
+          <Route path="/external-api" component={ExternalApi} />
           <Route exact path="/" component={Debate} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/account" component={Account} />
           <Route exact path="/debate" component={Debate} />
-<<<<<<< HEAD
-          <Route exact path="/newdebates" component={NewDebates} />
-=======
-          {/* <Route exact path="/newdebates" component={NewDebates} />
-          <Route exact path="/trending" component={Trending} /> */}
->>>>>>> a9a75f6d85c82362e158a701377128b9dc0a0d19
-        </main>
-      </div>
-    </Router>
+        </Switch>
+      </Container>
+      <Footer />
+    </div>
   );
-}
+};
 
 export default App;
+
+// function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <Navbar />
+//         <main>
+//           <Route exact path="/" component={Debate} />
+//           <Route exact path="/login" component={Login} />
+//           <Route exact path="/account" component={Account} />
+//           <Route exact path="/debate" component={Debate} />
+//           {/* <Route exact path="/newdebates" component={NewDebates} />
+//           <Route exact path="/trending" component={Trending} /> */}
+//         </main>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
