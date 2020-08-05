964491edc2fd604b46d0e9680dc4d0966069896f
marlon20m <marlonmora.ndr@gmail.com>
2020-08-04 21:22:55 -0500

    getting Auth0 data for homepage using Navbars

--- client/src/pages/Debate/Debate.js
+++ client/src/pages/Debate/Debate.js
@@ -1,43 +1,44 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Vote from "../../components/Vote";
 import Argument from "../../components/Argument";
 import Comment from "../../components/Comment";
 import API from "../../utils/api";
 import "./Debate.css";
-
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Debate = () => {
-const [topic, setTopic] = useState ("")
-const [incumbentbody, setIncumbentbody] = useState ("") 
-const [challengerbody, setChallengerbody] = useState ("") 
+  const [topic, setTopic] = useState("")
+  const [incumbentbody, setIncumbentbody] = useState("")
+  const [challengerbody, setChallengerbody] = useState("")
+  const { isAuthenticated, user } = useAuth0();
 
-useEffect(()=>{
-  API.debates()
-  .then(results =>{
-    console.log(results.data.topic)
-    setTopic(results.data.topic)
-    setIncumbentbody(results.data.incumbent_body)
-    setChallengerbody(results.data.challenger_body)
+  useEffect(() => {
+    API.debates()
+      .then(results => {
+        console.log(results.data.topic)
+        setTopic(results.data.topic)
+        setIncumbentbody(results.data.incumbent_body)
+        setChallengerbody(results.data.challenger_body)
+      })
+
   })
-  
-})
   return (
     <div className="form">
       <header className="appHeader">
         <h1>Triggered</h1>
       </header>
-      
-        <div>
-          <form>
-          <Argument topic={topic} incumbent_body={incumbentbody} challenger_body={challengerbody}/>
+      {isAuthenticated ? <> Hi {user.name} </> : null}
+      <div>
+        <form>
+          <Argument topic={topic} incumbent_body={incumbentbody} challenger_body={challengerbody} />
           <Vote />
           <Comment />
-          </form>
-        </div>
-      
-      
+        </form>
       </div>
-    
+
+
+    </div>
+
   );
 };
 
--- scripts/triggeredDB.sql
+++ scripts/triggeredDB.sql
@@ -14,16 +14,18 @@ CREATE TABLE users (
 
 CREATE TABLE debates (
   id INT NOT NULL AUTO_INCREMENT,
+  user_id VARCHAR(40) NOT NULL
   incumbent_username VARCHAR(20) NOT NULL,
   challenger_username VARCHAR(20) NULL,
   topic VARCHAR(100) NOT NULL,
   incumbent_body VARCHAR (280) NOT NULL,
-  challenger_body VARCHAR (280) NULL,
+  challenger_body VARCHAR (280) NOT NULL,
   incumbent_votes INT NULL,
   challenger_votes INT NULL,
   voter_usernames VARCHAR (20) NULL,
   comments VARCHAR (280) NULL,
-  created_at DATETIME NOT NULL,
+  createdAt DATETIME NOT NULL,
+  updatedAt DATETIME NOT NULL,
   PRIMARY KEY (id)
 );
 
@@ -36,11 +38,9 @@ VALUES ("Vader911", "empire@yahoo.com", "123");
 INSERT INTO users (username, email, password_KEY)
 VALUES ("Batman", "batcave@gmail.com", "123");
 
-INSERT INTO debates (incumbent_username, topic, incumbent_body, challenger_body, created_at)
-VALUES ("Cranekick50", "who is the better superhero???", "batman has lots of gadgets", "superman has super powers", "2020-01-01 12:00:00");
-
-INSERT INTO debates (incumbent_username, topic, incumbent_body, challenger_body, created_at)
-VALUES ("Batman", "Which gadget should I use?", "batrang?", "grappling hook?", "2020-01-02 14:00:00");
-
-INSERT INTO debates (incumbent_username, topic, incumbent_body, challenger_body, created_at)
-VALUES ("Vader911", "who should I put in carbonite first?", "Luke Skywalker", "Han Solo", "2020-01-03 15:00:00");
+INSERT INTO debates (incumbent_username, topic, incumbent_body, challenger_body, createdAt, updatedAt)
+VALUES ("Cranekick50", "who is the better superhero???", "batman has lots of gadgets", "superman has super powers", "2020-01-01 12:00:00", "2020-01-01 12:00:00");
+INSERT INTO debates (incumbent_username, topic, incumbent_body, challenger_body, createdAt, updatedAt)
+VALUES ("Batman", "Which gadget should I use?", "batrang?", "grappling hook?", "2020-01-02 14:00:00", "2020-01-02 14:00:00");
+INSERT INTO debates (incumbent_username, topic, incumbent_body, challenger_body, createdAt, updatedAt)
+VALUES ("Vader911", "who should I put in carbonite first?", "Luke Skywalker", "Han Solo", "2020-01-03 15:00:00", "2020-01-03 15:00:00");
