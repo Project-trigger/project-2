c09d5377bf46daf42a1d735c3556f7276bce84ed
Michael Srithapin <michael.srithapin@gmail.com>
2020-08-05 23:58:14 -0500

    fixed issue with argument disappearing

--- client/src/components/Loginbutton.js
+++ client/src/components/Loginbutton.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { MenuItem } from "@material-ui/core";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
   return (
-    <Button
+    <Link
       onClick={() => loginWithRedirect()}
       palette="primary: deepOrange"
       className="btn-margin"
     >
-      Log In
-    </Button>
+      <MenuItem>Log In</MenuItem>
+    </Link>
   );
 };
 
